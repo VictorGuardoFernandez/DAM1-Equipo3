7675794438c6d663cee425941eda7d715154ea6c
Asier542 <asierrodriguezormaechea@gmail.com>
2024-05-10 18:00:23 +0200

    Swing de java

---
+++ ejemplo/src/main/java/asierrodriguez/ejemplo/Ejemplo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asierrodriguez.ejemplo;
+
+/**
+ *
+ * @author DAM105
+ */
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        Menu m1=new Menu();
+       
+    }
+}
