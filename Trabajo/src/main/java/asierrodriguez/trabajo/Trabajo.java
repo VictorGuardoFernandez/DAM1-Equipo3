66e82df61df302f85d8f37dc92d5bdabcc0a2d83
Víctor Guardo Férnández <DAM128@iesmhp.local>
2024-05-13 19:40:21 +0200

    Interfaz Grafica 2ª prueba
    Signed-off-by: Víctor Guardo Férnández <DAM128@iesmhp.local>

---
+++ Trabajo/src/main/java/asierrodriguez/trabajo/Trabajo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asierrodriguez.trabajo;
+
+/**
+ *
+ * @author DAM105
+ */
+public class Trabajo {
+
+    public static void main(String[] args) {
+     Menu m1=new Menu();
+     Sesion s1=new Sesion();
+     s1.setVisible(true);
+     s1.setLocationRelativeTo(null);
+    }
+}
